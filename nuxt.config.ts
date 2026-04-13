122f39319ee6789d10806d058a2c4402a4f90423
Vladislav Adamets <adametsderschopfer@yandex.ru>
2026-04-13 23:30:42 +0200

    Fix favicon configuration and update favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,8 @@ export default defineNuxtConfig({
         { name: 'yandex-verification', content: '408e9c6c152e0036' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=20260413' },
         { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
