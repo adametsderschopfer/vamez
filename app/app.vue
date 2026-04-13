71431439d5fcc9ff1ec8f8a53ba8d50444931ee9
Vladislav Adamets <adametsderschopfer@yandex.ru>
2026-04-13 15:28:58 +0200

    Add Yandex.Metrika counter

--- app/app.vue
+++ app/app.vue
@@ -15,6 +15,19 @@ useHead({
   },
   title: computed(() => t('seo.ogTitle')),
   link: [{ rel: 'canonical', href: canonicalUrl }],
+  script: [
+    {
+      key: 'yandex-metrika',
+      children: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108521187','ym');ym(108521187,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`
+    }
+  ],
+  noscript: [
+    {
+      key: 'yandex-metrika-noscript',
+      children:
+        '<div><img src="https://mc.yandex.ru/watch/108521187" style="position:absolute;left:-9999px;" alt="" /></div>'
+    }
+  ],
   meta: [
     { name: 'description', content: computed(() => t('seo.description')) },
     { name: 'image', content: socialImageUrl },
